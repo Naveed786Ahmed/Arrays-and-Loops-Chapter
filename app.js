2664c15544b8f012a5a7e79ca7972b68c3cbf8e6
Naveed786Ahmed <138322439+Naveed786Ahmed@users.noreply.github.com>
2023-12-30 11:16:20 +0500

    Chapter Arrays & Loops

---
+++ app.js
@@ -0,0 +1,121 @@
+//Question # 01
+
+// let matrix = [[],[],[]];
+// console.log(matrix);
+
+//Question # 02
+
+// let matrix = [[0, 1, 2, 3],
+//               [1, 0, 1, 2],
+//               [2, 1, 0, 1]];
+
+// for(let i = 0; i < matrix.length; i++){
+//     console.log(matrix[i].join(" "));
+// }
+
+//Question # 03
+
+// for(let i = 1; i < 11; i++){
+//     console.log(i);
+// }
+
+//Question # 04
+
+// let table = +prompt("Enter a number to show its multiplication table", 2);
+// let lengths = +prompt("Enter length multiplication table", 10);
+
+// document.write(`<h1>Multiplication table of ${table} Length ${lengths}:</h1>`);
+
+// for(let i = 1; i < lengths + 1; i++){
+//     document.write(`${table} x ${i} = ${table * i} <br>`)
+// }
+
+//Question # 05
+
+// let fruits = ["apple", "bannana", "mango", "orange", "strawberry"];
+
+// for(let i = 0; i < fruits.length; i++){
+//     document.write(`${fruits[i]} <br>`);
+// }
+
+// for(let i = 0; i < fruits.length; i++){
+//     document.write(`Element at index ${i} is ${fruits[i]} <br>`)
+// }
+
+//Question # 06
+
+// let Counting = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+// let Reverse_Counting = [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+// let Even = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+// let Odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+// let Series = ["2k", "4k", "6k", "8k", "10k", "12k", "14k", "16k", "18k", "20k"]
+
+// document.write(`<h3>Counting:</h3> ${Counting}`)
+// document.write(`<h3>Reverse Counting:</h3> ${Reverse_Counting}`)
+// document.write(`<h3>Even:</h3> ${Even}`)
+// document.write(`<h3>Odd:</h3> ${Odd}`)
+// document.write(`<h3>Series:</h3> ${Series}`)
+
+//Question # 08
+
+// let A = [24, 53, 78, 91, 12];
+// let max = 91;
+
+// for(let i = 0; i < A.length; i++){
+//     if(A[i] > max){
+//         max = A[i]
+//     }
+// }
+// document.write(`Array items: ${A}<br>`)
+// document.write(`The largest number is ${max}`)
+
+//Question # 09
+
+// let A = [24, 53, 78, 91, 12];
+// let max = 91;
+
+// for(let i = 0; i < A.length; i++){
+//     if(A[i] < max){
+//         max = A[i]
+//     }
+// }
+// document.write(`Array items: ${A}<br>`)
+// document.write(`The Samllest number is ${max}`)
+
+//Question # 10
+
+// let arr = []
+// let arr2 = []
+
+// for(let i = 1; i < 101; i++){
+//     arr.push(i);
+// }
+
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] % 5 == 0){;
+//         arr2.push(arr[i])
+//     }
+// }
+
+// document.write(`Array items: ${arr}<br>`)
+// document.write(arr2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
